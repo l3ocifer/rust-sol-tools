469828dec8937e766f81531e7ac15e5f300e0527
l3ocifer <lpask001@gmail.com>
2024-11-09 01:51:02 -0500

    fix main.rs imports and main functions to resolve compilation errors

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
 use leptos::*;
+use crate::app::App;
+
 #[cfg(feature = "ssr")]
-use leptos_actix::*;
+use leptos_actix::{generate_route_list, LeptosRoutes};
 #[cfg(feature = "ssr")]
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App as ActixApp, HttpServer, middleware::Logger};
 #[cfg(feature = "ssr")]
-use sol_tools::routes::{metadata::upload_metadata, contract::create_token_route};
+use crate::routes::{metadata::upload_metadata, contract::create_token_route};
 
 #[cfg(feature = "ssr")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    use leptos_config::get_configuration;
+
     let conf = get_configuration(None).await.unwrap();
     let addr = conf.leptos_options.site_addr;
 
     HttpServer::new(move || {
         let leptos_options = conf.leptos_options.clone();
         
-        App::new()
+        ActixApp::new()
+            .wrap(Logger::default())
             .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
             .service(upload_metadata)
             .service(create_token_route)
             .leptos_routes(
                 leptos_options.clone(),
-                generate_route_list(|| view! { <sol_tools::app::App/> }),
-                || view! { <sol_tools::app::App/> }
+                generate_route_list(|cx| view! { cx, <App/> }),
+                |cx| view! { cx, <App/> }
             )
             .app_data(web::Data::new(leptos_options))
     })
@@ -31,37 +36,10 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    use crate::App;
+#[cfg(not(feature = "ssr"))]
+#[wasm_bindgen::prelude::wasm_bindgen(start)]
+pub fn main() {
     use leptos::*;
 
-    leptos::mount_to_body(|| {
-        view! { <App/> }
-    });
-}
-
-#[cfg(not(target_arch = "wasm32"))]
-fn main() {
-    use actix_files::Files;
-    use actix_web::{middleware, App as ActixApp, HttpServer};
-    use crate::routes::create_token_route;
-
-    #[actix_web::main]
-    async fn server_main() -> std::io::Result<()> {
-        HttpServer::new(|| {
-            ActixApp::new()
-                .wrap(middleware::Logger::default())
-                .service(create_token_route)
-                .service(Files::new("/", "./static").index_file("index.html"))
-            // ... Add other services or middleware
-        })
-        .bind("127.0.0.1:3000")?
-        .run()
-        .await
-    }
-
-    if let Err(e) = server_main().await {
-        eprintln!("Server error: {}", e);
-    }
+    leptos::mount_to_body(|cx| view! { cx, <App/> });
 }
