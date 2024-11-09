7aa30ba6167b0c066b0a4a8280570fed04dfe1d4
l3ocifer <lpask001@gmail.com>
2024-11-09 16:34:22 -0500

    add console log dependency and clean up imports

--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,12 @@ pub mod utils;
 pub fn hydrate() {
     use app::*;
     use leptos::*;
-    use wasm_bindgen::prelude::*;
 
     _ = console_error_panic_hook::set_once();
     
-    // Initialize wasm_bindgen
     #[cfg(debug_assertions)]
-    console_log::init_with_level(log::Level::Debug).expect("Failed to initialize logger");
+    console_log::init_with_level(log::Level::Debug)
+        .expect("Failed to initialize logger");
     
     mount_to_body(move || {
         view! { <App/> }
